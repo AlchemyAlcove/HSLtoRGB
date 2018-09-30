8082ab8cfa1700dd5b18ae24108c461f9a341f79
Mike Hoffert <mike.hoffert@gmail.com>
2018-09-30 09:23:16 -0600

    HSL to RGB logic

--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
-export default function(rgb) {
-  let r = rgb[0] / 255;
-  let g = rgb[1] / 255;
-  let b = rgb[2] / 255;
-  let max = Math.max(r, g, b);
-  let min = Math.min(r, g, b);
-  let delta = max - min;
-  let h;
-  let s;
-  let l;
+export default function(hsl) {
+  var h = hsl[0] / 360;
+  var s = hsl[1] / 100;
+  var l = hsl[2] / 100;
+  var t1;
+  var t2;
+  var t3;
+  var rgb;
+  var val;
 
-  if(max === min) {
-    h = 0;
-  } else if(r === max) {
-    h = (g - b) / delta;
-  } else if(g === max) {
-    h = 2 + (b - r) / delta;
-  } else if(b === max) {
-    h = 4 + (r - g) / delta;
+  if(s === 0) {
+    val = l * 255;
+    return([Math.round(val), Math.round(val), Math.round(val)]);
   }
 
-  h = Math.min(h * 60, 360);
-
-  if(h < 0) {
-    h += 360;
-  }
-
-  l = (min + max) / 2;
-
-  if(max === min) {
-    s = 0;
-  } else if(l <= 0.5) {
-    s = delta / (max + min);
+  if(l < 0.5) {
+    t2 = l * (1 + s);
   } else {
-    s = delta / (2 - max - min);
+    t2 = l + s - l * s;
   }
 
-  return([h, Math.floor(s * 100), Math.floor(l * 100)]);
+  t1 = 2 * l - t2;
+
+  rgb = [0, 0, 0];
+  for(var i = 0; i < 3; i++) {
+    t3 = h + 1 / 3 * -(i - 1);
+    if (t3 < 0) {
+      t3++;
+    }
+    if (t3 > 1) {
+      t3--;
+    }
+
+    if(6 * t3 < 1) {
+      val = t1 + (t2 - t1) * 6 * t3;
+    } else if(2 * t3 < 1) {
+      val = t2;
+    } else if(3 * t3 < 2) {
+      val = t1 + (t2 - t1) * (2 / 3 - t3) * 6;
+    } else {
+      val = t1;
+    }
+
+    rgb[i] = Math.round(val * 255);
+  }
+
+  return(rgb);
 }
--- test/index.test.js
+++ test/index.test.js
@@ -5,21 +5,21 @@ test("valid black", () => {
 });
 
 test("valid white", () => {
-  expect(Convert([255, 255, 255])).toEqual([0, 0, 100]);
+  expect(Convert([0, 0, 100])).toEqual([255, 255, 255]);
 });
 
 test("valid red", () => {
-  expect(Convert([255, 0, 0])).toEqual([0, 100, 50]);
+  expect(Convert([0, 100, 50])).toEqual([255, 0, 0]);
 });
 
 test("valid green", () => {
-  expect(Convert([0, 255, 0])).toEqual([120, 100, 50]);
+  expect(Convert([120, 100, 50])).toEqual([0, 255, 0]);
 });
 
 test("valid blue", () => {
-  expect(Convert([0, 0, 255])).toEqual([240, 100, 50]);
+  expect(Convert([240, 100, 50])).toEqual([0, 0, 255]);
 });
 
 test("valid midnight blue", () => {
-  expect(Convert([44, 62, 80])).toEqual([210, 29, 24]);
+  expect(Convert([210, 29, 24])).toEqual([43, 61, 79]);
 });
